f8c019ab62163bf32d0ccb7f401e7c9f3a5cb505
a28910382 <wangweixiang@wangweijiedeMacBook-Pro.local>
2016-11-26 14:34:28 +0800

    inside

---
+++ inside/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-14257672_1087867107948409_2012413627_n",
+      "name": "14257672_1087867107948409_2012413627_n",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.34687072930650586,
+        "pitch": 0.024975757149045208,
+        "fov": 1.1060087463985706
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
